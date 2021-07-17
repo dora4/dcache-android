a80adb9adef235c7eb570d4bd75f6bf48f3b5320
dora <924666990@qq.com>
2021-07-17 16:00:23 +0800

    添加CacheFactory的注释

--- dcache/src/main/java/dora/cache/factory/CacheFactory.kt
+++ dcache/src/main/java/dora/cache/factory/CacheFactory.kt
@@ -4,11 +4,23 @@ import dora.db.builder.Condition
 
 interface CacheFactory<M> {
 
+    /**
+     * 该类型数据库orm框架的一些初始化操作在这里进行。
+     */
     fun init()
 
+    /**
+     * 从数据库中加载数据到内存。
+     */
     fun queryCache(condition: Condition) : M?
 
+    /**
+     * 移除旧的数据库缓存。
+     */
     fun removeOldCache(condition: Condition)
 
+    /**
+     * 将最新的数据缓存到数据库。
+     */
     fun addNewCache(model: M)
 }

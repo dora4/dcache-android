aa458b4791bab598b1bb0684be05f4a4186ebc66
JackWHLiu <dora924666990@gmail.com>
2022-03-23 21:37:39 +0800

    optimize ListResultAdapter

--- dcache/src/main/java/dora/cache/data/adapter/ListResultAdapter.kt
+++ dcache/src/main/java/dora/cache/data/adapter/ListResultAdapter.kt
@@ -6,7 +6,8 @@ import dora.http.DoraListCallback
 class ListResultAdapter<M, R : Result<M>>(val callback: DoraListCallback<M>) : DoraCallback<R>() {
 
     override fun onSuccess(model: R) {
-        callback.onSuccess(model.getRealModel() as List<M>)
+        callback.onSuccess(if (model.getRealModel() != null &&
+                (model.getRealModel() is List<*>)) model.getRealModel() as List<M> else arrayListOf())
     }
     override fun onFailure(code: Int, msg: String?) {
         callback.onFailure(code, msg)

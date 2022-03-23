a9b97b6dfc3ae11b39211d971256966d08e8babe
JackWHLiu <dora924666990@gmail.com>
2022-03-23 19:08:01 +0800

    merge JRetrofitManager into RetrofitManager

--- dcache/src/main/java/dora/http/retrofit/RetrofitManager.kt
+++ dcache/src/main/java/dora/http/retrofit/RetrofitManager.kt
@@ -6,63 +6,61 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.util.*
 import kotlin.collections.HashMap
 
-abstract class RetrofitManager protected constructor() {
+object RetrofitManager {
 
-    private var clientBuilder: OkHttpClient.Builder = OkHttpClient.Builder()
+    private lateinit var client: OkHttpClient
+    private var retrofitMap: MutableMap<Class<*>, Retrofit> = HashMap()
+    private var urlMap: MutableMap<Class<*>, String> = HashMap()
+    private var config: Config = Config()
 
-    init {
-        client = createHttpClient()
-        initBaseUrl(client)
-        config = Config()
+    fun getConfig() : Config {
+        return config
     }
 
-    protected abstract fun createHttpClient(): OkHttpClient
-
-    protected abstract fun initBaseUrl(client: OkHttpClient)
-
-    companion object {
-
-        private var retrofitMap: MutableMap<Class<*>, Retrofit> = HashMap()
-        private var urlMap: MutableMap<Class<*>, String> = HashMap()
-        internal lateinit var client: OkHttpClient
-        internal lateinit var config: Config
-
-        fun init(block: Config.() -> Unit) {
-            block(config)
-        }
-
-        fun <T : ApiService> getService(clazz: Class<T>): T {
-            val retrofit: Retrofit?
-            if (retrofitMap.containsKey(clazz)) {
-                retrofit = retrofitMap[clazz]
-                return retrofit!!.create(clazz)
-            } else {
-                retrofit = Retrofit.Builder()
-                        .baseUrl(Objects.requireNonNull(urlMap[clazz]))
-                        .addConverterFactory(GsonConverterFactory.create())
-                        .client(client)
-                        .build()
-                retrofitMap[clazz] = retrofit
-            }
-            return retrofit.create(clazz)
-        }
+    fun initConfig(block: Config.() -> Unit) {
+        block(config)
     }
 
-    inner class Config {
+    fun <T : ApiService> getService(clazz: Class<T>): T {
+        val retrofit: Retrofit?
+        if (retrofitMap.containsKey(clazz)) {
+            retrofit = retrofitMap[clazz]
+            return retrofit!!.create(clazz)
+        } else {
+            retrofit = Retrofit.Builder()
+                    .baseUrl(Objects.requireNonNull(urlMap[clazz]))
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
+                    .build()
+            retrofitMap[clazz] = retrofit
+        }
+        return retrofit.create(clazz)
+    }
+
+
+    class Config {
+
+        private var client = OkHttpClient()
+        val builder = OkHttpClient.Builder()
+
+        fun setClient(client: OkHttpClient) : Config {
+            this.client = client
+            return this
+        }
+
+        fun getClient() : OkHttpClient {
+            return client
+        }
+
+        fun okhttp(block: OkHttpClient.Builder.() -> OkHttpClient) {
+            client = block(builder)
+        }
 
         /**
          * 请在子类的initBaseUrl()中进行注册，可以注册多个url地址。
          */
         fun registerBaseUrl(serviceClazz: Class<out ApiService>, baseUrl: String) = apply {
             urlMap[serviceClazz] = baseUrl
-        }
-
-        /**
-         * 只配置一个。
-         */
-        fun okhttp(block: OkHttpClient.Builder.() -> OkHttpClient.Builder) = apply {
-            clientBuilder = block(clientBuilder)
-            client = clientBuilder.build()
         }
     }
 }

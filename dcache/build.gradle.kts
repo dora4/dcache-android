b31bb6cc3ded75253917b57973bbb9b775aa403f
dora4 <71242257+dora4@users.noreply.github.com>
2023-06-25 03:22:46 +0800

    优化drop table的逻辑，不再需要依赖okhttp和retrofit等网络相关库

--- dcache/build.gradle.kts
+++ dcache/build.gradle.kts
@@ -29,14 +29,14 @@ dependencies {
 
     api("com.squareup.okhttp3:okhttp:4.8.0")
     api("com.squareup.retrofit2:converter-gson:2.8.1")
-    api("com.squareup.retrofit2:retrofit:2.9.0")
+    api("com.squareup.retrofit2:retrofit:2.8.1")
     api("com.squareup.retrofit2:adapter-rxjava2:2.8.1")
-    api("io.reactivex.rxjava2:rxjava:2.2.4")
-    api("io.reactivex.rxjava2:rxandroid:2.1.0")
+    api("io.reactivex.rxjava2:rxjava:2.0.4")
+    api("io.reactivex.rxjava2:rxandroid:2.0.1")
 
-    api("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.8.21")
-    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
-    api("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    api("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.5.10")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.2")
 
     api("com.tencent:mmkv:1.2.16")
 }
@@ -48,7 +48,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.dora4"
                 artifactId = "dcache-android"
-                version = "1.7.7"
+                version = "1.7.8"
             }
         }
     }

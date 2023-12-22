8d7d9af0e23530d375be2f7af9eebc9c0ff44ce2
dora <dora924666990@gmail.com>
2023-12-22 23:53:35 +0800

    升级版本号

--- dcache/build.gradle.kts
+++ dcache/build.gradle.kts
@@ -50,7 +50,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.dora4"
                 artifactId = "dcache-android"
-                version = "2.1.1"
+                version = "2.1.2"
             }
         }
     }

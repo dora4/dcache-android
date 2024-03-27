c28e77559eb039153591e7684398880cc926bcc4
dora <dora924666990@gmail.com>
2024-03-27 10:18:14 +0800

    fetch函数新增空的默认值，方便调用处不传

--- dcache/src/main/java/dora/cache/data/fetcher/IListFlowDataFetcher.kt
+++ dcache/src/main/java/dora/cache/data/fetcher/IListFlowDataFetcher.kt
@@ -22,7 +22,7 @@ interface IListFlowDataFetcher<M> {
     /**
      * 开始抓取数据。
      */
-    fun fetchListData(description: String?, listener: OnLoadStateListener? = OnLoadStateListenerImpl()): StateFlow<MutableList<M>>
+    fun fetchListData(description: String? = "", listener: OnLoadStateListener? = OnLoadStateListenerImpl()): StateFlow<MutableList<M>>
 
     /**
      * 获取livedata。

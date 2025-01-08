b8002e965cff15207597eb914ae862874dfee6fd
dora <dora924666990@gmail.com>
2025-01-08 20:38:11 +0800

    Switching threads no longer requires the Activity parameter.(https://github.com/dora4/dcache-android/issues/22)
    (cherry picked from commit 6e22768206ad1bdad79384291a5331264edbc6b3)

--- dcache/src/main/java/dora/http/coroutine/ThreadSwitchContinuation.kt
+++ dcache/src/main/java/dora/http/coroutine/ThreadSwitchContinuation.kt
@@ -1,17 +1,18 @@
 package dora.http.coroutine
 
-import android.app.Activity
+import android.os.Handler
+import android.os.Looper
 import kotlin.Result
 import kotlin.coroutines.Continuation
 import kotlin.coroutines.CoroutineContext
 
-class ThreadSwitchContinuation<T>(private val activity: Activity, private val continuation:
+class ThreadSwitchContinuation<T>(private val continuation:
     Continuation<T>) : Continuation<T> {
 
     override val context: CoroutineContext = continuation.context
 
     override fun resumeWith(result: Result<T>) {
-        activity.runOnUiThread {
+        Handler(Looper.getMainLooper()).post {
             continuation.resumeWith(result)
         }
     }

6eb2eb06ff618ea7883ef68f2c0810fea6e77499
dora <dora924666990@gmail.com>
2025-06-23 15:25:29 +0800

    The @Since annotation supports being configured at the class level.(https://github.com/dora4/dcache-android/issues/38)

--- dcache/build.gradle.kts
+++ dcache/build.gradle.kts
@@ -51,7 +51,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.dora4"
                 artifactId = "dcache-android"
-                version = "3.3.12"
+                version = "3.4.0"
             }
         }
     }
--- dcache/src/main/java/dora/db/table/Since.kt
+++ dcache/src/main/java/dora/db/table/Since.kt
@@ -1,9 +1,9 @@
 package dora.db.table
 
 /**
- * Marks the starting database version for the table's column name.
- * 简体中文：标记表的列名开始的数据库版本。
+ * Marks the starting database version for the table's name and table's column name.
+ * 简体中文：标记表名和表的列名开始的数据库版本。
  */
-@Target(AnnotationTarget.FIELD)
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FIELD)
 @Retention(AnnotationRetention.SOURCE)
 annotation class Since(val version: Int)
